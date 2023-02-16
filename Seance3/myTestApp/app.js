3690f2ff1c9e9a99df6c232ed74fb99f4a2bab3d
jasserat <jasser.atouani@etudiant-fst.utm.tn>
2023-02-16 22:22:03 +0100

    Atelier 2 update

--- Seance3/myTestApp/app.js
+++ Seance3/myTestApp/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var osRouter = require('./routes/os');
 var app = express();
 
 // view engine setup
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/os',osRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
