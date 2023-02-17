eeae931a9716ca2c0553982db9179c69f67c11d8
jasserat <jasser.atouani@etudiant-fst.utm.tn>
2023-02-17 16:40:53 +0100

    DELETE & PUT

--- Seance4/Routes/contacts.js
+++ Seance4/Routes/contacts.js
@@ -26,4 +26,20 @@ router.post('/', (req,res,next)=>{
     });
 });
 
+router.put('/:id', (req, res) => {
+    contact.findByIdAndUpdate(req.params.id, req.body).then((data) => {
+        res.json({ message: 'Contact Updated Successfully' })
+    }).catch((err) => {
+        res.json({ message: 'Contact Not Updated' })
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    contact.findByIdAndRemove(req.params.id).then((data) => {
+        res.json({ message: 'Contact Deleted Successfully' })
+    }).catch((err) => {
+        res.json({ message: 'Contact Not Deleted' })
+    })
+});
+
 module.exports = router;
